add86d34c5e22dfa670412ab0bf1f73b82076e3c
TheEddyChang <ehchang58@yahoo.com>
2015-10-29 21:08:02 -0700

    jquery

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+
+ $('body').click(function(){
+     $('.img').fadeOut('slow');
+ });
+	
+$('body').mouseover(function(){
+    $('.places').animate({height: "500px", opacity: .08});
+});
+
+
+
+/*$(window).scroll(function() {
+	$('.places').fadeOut('slow');*/
+
+
+
+});						
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
